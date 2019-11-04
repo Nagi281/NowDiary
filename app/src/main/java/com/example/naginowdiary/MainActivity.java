efcf54a82a5bd5ea6e2d6ad8dddf6219d6eed55c
Nagi281 <n.h.quangtinh281@gmail.com>
2019-11-04 12:46:18 +0700

    Update logout

--- app/src/main/java/com/example/naginowdiary/MainActivity.java
+++ app/src/main/java/com/example/naginowdiary/MainActivity.java
@@ -205,6 +205,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 }
                                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                                finish();
                             }
                         });
             }
